f6849d78f7ce3b1e8d27d6265a579b8394e2b54a
Your Name <you@example.com>
2023-07-02 22:50:50 +0400

    lesson two

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -3,66 +3,71 @@
 namespace App\Manager;
 
 use App\Entity\User;
-use Doctrine\Common\Collections\Criteria;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
 
 class UserManager
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
     }
 
-    public function create(string $login): User
+    public function saveUser(string $login): ?int
     {
         $user = new User();
         $user->setLogin($login);
-        $user->setCreatedAt();
-        $user->setUpdatedAt();
         $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user->getId();
+    }
+
+    public function updateUser(int $userId, string $login): ?User
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = $this->entityManager->getRepository(User::class);
+        /** @var User $user */
+        $user = $userRepository->find($userId);
+        if ($user === null) {
+            return null;
+        }
+        $user->setLogin($login);
         $this->entityManager->flush();
 
         return $user;
     }
 
-    public function clearEntityManager(): void
+    public function deleteUserById(int $userId): bool
     {
-        $this->entityManager->clear();
+        /** @var UserRepository $userRepository */
+        $userRepository = $this->entityManager->getRepository(User::class);
+        /** @var User $user */
+        $user = $userRepository->find($userId);
+        if ($user === null) {
+            return false;
+        }
+        return $this->deleteUser($user);
     }
 
-    public function findUser(int $id): ?User
+    public function deleteUser(User $user): bool
     {
-        $repository = $this->entityManager->getRepository(User::class);
-        $user = $repository->find($id);
-
-        return $user instanceof User ? $user : null;
-    }
-
-    public function subscribeUser(User $author, User $follower): void
-    {
-        $author->addFollower($follower);
-        $follower->addAuthor($author);
+        $this->entityManager->remove($user);
         $this->entityManager->flush();
+
+        return true;
     }
 
     /**
      * @return User[]
      */
-    public function findUsersByLogin(string $name): array
+    public function getUsers(int $page, int $perPage): array
     {
-        return $this->entityManager->getRepository(User::class)->findBy(['login' => $name]);
-    }
+        /** @var UserRepository $userRepository */
+        $userRepository = $this->entityManager->getRepository(User::class);
 
-    /**
-     * @return User[]
-     */
-    public function findUsersByCriteria(string $login): array
-    {
-        $criteria = Criteria::create();
-        $criteria->andWhere(Criteria::expr()->eq('login', $login));
-        /** @var EntityRepository $repository */
-        $repository = $this->entityManager->getRepository(User::class);
-
-        return $repository->matching($criteria)->toArray();
+        return $userRepository->getUsers($page, $perPage);
     }
 }
